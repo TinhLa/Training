003ad8c041e70fe6e6a03bac17de50fd39bb965e
La Trung Tinh <lttinh@tma.com.vn>
2018-09-10 17:42:56 +0700

    change MainActivity to kotlin

---
+++ app/src/main/java/com/training/tinhla/training/mainscreen/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.training.tinhla.training.mainscreen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.training.tinhla.training.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
