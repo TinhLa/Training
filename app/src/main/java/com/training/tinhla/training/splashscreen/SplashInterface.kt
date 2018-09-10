6758fbdd27be9c3c4d9283a4f92dd1d39002deb3
La Trung Tinh <lttinh@tma.com.vn>
2018-09-10 15:30:45 +0700

    MVP structure example

---
+++ app/src/main/java/com/training/tinhla/training/splashscreen/SplashInterface.kt
@@ -0,0 +1,8 @@
+package com.training.tinhla.training.splashscreen
+
+class SplashInterface {
+    interface Presenter
+
+    interface View
+}
+
---
+++ app/src/main/java/com/training/tinhla/training/splashscreen/SplashPresenterImpl.kt
@@ -0,0 +1,8 @@
+package com.training.tinhla.training.splashscreen
+
+class SplashPresenterImpl (var view: SplashInterface.View) : SplashInterface.Presenter{
+
+    init {
+
+    }
+}
