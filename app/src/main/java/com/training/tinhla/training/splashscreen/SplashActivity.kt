c3c2d63eb273b87387f1c26321d71c9899288c27
Vo Ngoc Hanh <nghiamy15@gmail.com>
2018-09-13 14:23:43 +0700

    Handle 2 scrollviews

--- app/src/main/java/com/training/tinhla/training/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/training/tinhla/training/splashscreen/SplashActivity.kt
@@ -3,12 +3,12 @@ package com.training.tinhla.training.splashscreen
 import android.content.Context
 import android.graphics.Rect
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
+import android.support.v4.widget.NestedScrollView
 import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewTreeObserver
 import android.widget.ScrollView
 import com.training.tinhla.training.R
 import com.training.tinhla.training.basemodel.BaseActivity
@@ -18,8 +18,8 @@ class SplashActivity : BaseActivity(), SplashInterface.View {
     @Inject
     lateinit var presenter: SplashInterface.Presenter
 
-    lateinit var sv_parent: ScrollView
-    lateinit var sv_child : ScrollView
+    lateinit var svParent: ScrollView
+    lateinit var svChild : NestedScrollView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,44 +28,103 @@ class SplashActivity : BaseActivity(), SplashInterface.View {
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        sv_parent = findViewById(R.id.sv_parent)
-        sv_child = findViewById(R.id.sv_child)
+        svParent = findViewById(R.id.sv_parent)
+        svChild = findViewById(R.id.sv_child)
 
-        var childRect = Rect()
         var childLocs = intArrayOf(0,0)
-        var oldY = 0f
+        var oldY = -1f
 
-        sv_parent.setOnTouchListener(object : View.OnTouchListener{
+        var pRects = Rect()
+
+
+        svParent.viewTreeObserver.addOnScrollChangedListener(ViewTreeObserver.OnScrollChangedListener {
+            // determine location
+            svChild.getLocationInWindow(childLocs)
+
+            if(childLocs[1] <= 100){
+                svParent.isEnabled = false
+                svParent.scrollTo(0,500)
+            }
+        })
+
+        var oldY_parent = -1f
+
+        svParent.setOnTouchListener(object : View.OnTouchListener{
             override fun onTouch(v: View?, event: MotionEvent?): Boolean {
 
-                Log.d("LOG", "parent")
-                sv_child.parent.requestDisallowInterceptTouchEvent(false)
+                if (svChild.scrollY > 0) {
+                    svParent.requestDisallowInterceptTouchEvent(true)
+                    return true
+                }
+
+                svParent.getLocalVisibleRect(pRects)
+
+                when (event?.action) {
+                    MotionEvent.ACTION_DOWN -> oldY_parent = event?.y
+
+                    MotionEvent.ACTION_MOVE -> {
+                        if (!oldY_parent.equals(-1f)) {
+
+                            if (pRects.top >= 500) {
+                                var deltaY = event!!.y.minus(oldY_parent)
+
+                                //scroll down
+                                if (deltaY.compareTo(0f).equals(1)) {
+
+                                    if (svChild.scrollY == 0) {
+                                        svParent.requestDisallowInterceptTouchEvent(false)
+                                        return false
+                                    }
+                                }
+
+                                svParent.requestDisallowInterceptTouchEvent(true)
+                                return true
+                            }
+                        }
+                        oldY_parent = event?.y
+                    }
+
+                    MotionEvent.ACTION_UP -> oldY_parent = -1f
+                }
+
+                svParent.requestDisallowInterceptTouchEvent(false)
 
                 return false
             }
         })
 
-        sv_child.setOnTouchListener(object : View.OnTouchListener{
+        svChild.setOnTouchListener(object: View.OnTouchListener{
             override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+//                svChild.parent.requestDisallowInterceptTouchEvent(true)
 
-                sv_child.getLocationInWindow(childLocs)
-//                Log.d("LOG", "sv_child.locs: " + childLocs[0]+"/"+childLocs[1])
-                if(childLocs[1] <= 96)
-                    sv_child.parent.requestDisallowInterceptTouchEvent(false)
-                else{
-                    sv_parent.post(Runnable {
-                        //disable scrollview cha
-                        sv_child.parent.requestDisallowInterceptTouchEvent(true)
-//                        when (event?.action) {
-//                            MotionEvent.ACTION_DOWN ->
-//                                oldY = event.y
-//                            MotionEvent.ACTION_UP ->{
-//                                var deltaY = event.y - oldY
-//                                if(deltaY < 0)
-//                                    sv_parent.scrollBy(0, deltaY.toInt())
-//                            }
-//                        }
-                    })
+                svParent.getLocalVisibleRect(pRects)
+                if (pRects.top >= 500) {
+
+
+                    when (event?.action) {
+                        MotionEvent.ACTION_DOWN -> oldY = event?.y
+
+                        MotionEvent.ACTION_MOVE -> {
+                            if (!oldY.equals(-1f)) {
+
+                                var deltaY = event!!.y.minus(oldY)
+
+                                //scroll up
+                                if (deltaY.compareTo(0f).equals(1)) {
+
+                                    if (svChild.scrollY == 0) {
+                                        svParent.requestDisallowInterceptTouchEvent(false)
+                                        return true
+                                    }
+                                }
+
+                                svChild.parent.requestDisallowInterceptTouchEvent(true)
+                                return false
+                            }
+                        }
+
+                        MotionEvent.ACTION_UP -> oldY = -1f
+                    }
                 }
 
                 return false
@@ -75,9 +134,19 @@ class SplashActivity : BaseActivity(), SplashInterface.View {
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
-        val gv_main = findViewById<View>(R.id.main_gv)
 
-        sv_child.layoutParams.height = gv_main.height
+        Log.d("LOG", "height of action bar: " + supportActionBar?.height)
+
+        val gvMain = findViewById<View>(R.id.main_gv)
+        svChild.layoutParams.height = gvMain.height
+
+        var locs = intArrayOf(0,0)
+        svParent.getLocationOnScreen(locs)
+        Log.d("LOG", "locs: " + locs[1])
+
+
+        svParent.getLocationInWindow(locs)
+        Log.d("LOG", "locs: " + locs[1])
     }
 
     override fun getAppContext(): Context {
