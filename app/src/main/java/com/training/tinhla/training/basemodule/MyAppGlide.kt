6c62827aa076db9ff4d456528a6ee73a9d7acf64
La Trung Tinh <lttinh@tma.com.vn>
2018-09-19 15:03:15 +0700

    add glide into Project

---
+++ app/src/main/java/com/training/tinhla/training/basemodule/MyAppGlide.kt
@@ -0,0 +1,17 @@
+package com.training.tinhla.training.basemodule
+
+import android.content.Context
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.load.engine.cache.LruResourceCache
+
+
+
+@GlideModule
+open class MyAppGlide : AppGlideModule(){
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        val memoryCacheSizeBytes = 1024 * 1024 * 20 // 20mb
+        builder.setMemoryCache(LruResourceCache(memoryCacheSizeBytes.toLong()))
+    }
+}
