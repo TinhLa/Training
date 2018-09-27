238e06573fbd441826442e4aaecc5938f59d58c3
TMA\ntkhoinguyen <ntkhoinguyen@tma.com.vn>
2018-09-27 10:56:56 +0700

    set TemplateButton ( complete)

--- app/src/main/java/com/training/tinhla/training/nkhoi_srcollview/presenterImpl/JsonPresenterTemplateBtnImpl.kt
+++ app/src/main/java/com/training/tinhla/training/nkhoi_srcollview/presenterImpl/JsonPresenterTemplateBtnImpl.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.os.Build
 import android.support.annotation.RequiresApi
 import android.util.Log
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
@@ -22,6 +23,7 @@ class JsonPresenterTemplateBtnImpl (var context: Context) : JsonInterfaceTemplat
     private val dta : Data
     private val readJson : ReadJson
     private val  textjson : String
+    private var flag : Int =0
     init {
         readJson = ReadJson(context)
         textjson = readJson.readFileJsonFromAsset(context)
@@ -30,46 +32,101 @@ class JsonPresenterTemplateBtnImpl (var context: Context) : JsonInterfaceTemplat
 
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     override fun getListButton(viewGroup: ViewGroup) {
-        if(dta.templateBtn?.new?.size!!>1) checkEnableTwoButton(viewGroup, dta.templateBtn?.new?.get(0)?.state!!,dta.templateBtn?.new?.get(1)?.state!!) else setOneButton(viewGroup)
-//        if(dta.templateBtn?.complete?.size!!>1) setTwoButton() else setOneButton()
-//        if(dta.templateBtn?.expire?.size!!>1) setTwoButton() else setOneButton()
-//        if(dta.templateBtn?.fail?.size!!>1) setTwoButton() else setOneButton()
-//        if(dta.templateBtn?.cancel?.size!!>1) setTwoButton() else setOneButton()
+        if(dta.templateBtn?.new?.size!!>1)
+            checkEnableTwoButton(viewGroup,
+            dta.templateBtn?.new?.get(0)?.state!!,dta.templateBtn?.new?.get(0)?.icon!!,dta.templateBtn?.new?.get(0)?.textKey!!,
+            dta.templateBtn?.new?.get(1)?.state!!,dta.templateBtn?.new?.get(1)?.icon!!,dta.templateBtn?.new?.get(1)?.textKey!!)
+        else checkEnableOneButton(viewGroup,
+            dta.templateBtn?.new?.get(0)?.state!!,dta.templateBtn?.new?.get(0)?.icon!!,dta.templateBtn?.new?.get(0)?.textKey!!)
+
+
+        if(dta.templateBtn?.complete?.size!!>1)
+            checkEnableTwoButton(viewGroup,
+            dta.templateBtn?.complete?.get(0)?.state!!,dta.templateBtn?.complete?.get(0)?.icon!!,dta.templateBtn?.complete?.get(0)?.textKey!!,
+            dta.templateBtn?.complete?.get(1)?.state!!,dta.templateBtn?.complete?.get(1)?.icon!!,dta.templateBtn?.complete?.get(1)?.textKey!!)
+        else checkEnableOneButton(viewGroup,
+            dta.templateBtn?.complete?.get(0)?.state!!,dta.templateBtn?.complete?.get(0)?.icon!!,dta.templateBtn?.complete?.get(0)?.textKey!!)
+
+
+        if(dta.templateBtn?.expire?.size!!>1)
+            checkEnableTwoButton(viewGroup,
+            dta.templateBtn?.expire?.get(0)?.state!!,dta.templateBtn?.expire?.get(0)?.icon!!,dta.templateBtn?.expire?.get(0)?.textKey!!,
+            dta.templateBtn?.expire?.get(1)?.state!!,dta.templateBtn?.expire?.get(1)?.icon!!,dta.templateBtn?.expire?.get(1)?.textKey!!)
+        else checkEnableOneButton(viewGroup,
+            dta.templateBtn?.expire?.get(0)?.state!!,dta.templateBtn?.expire?.get(0)?.icon!!,dta.templateBtn?.expire?.get(0)?.textKey!!)
+
+
+        if(dta.templateBtn?.fail?.size!!>1)
+            checkEnableTwoButton(viewGroup,
+            dta.templateBtn?.fail?.get(0)?.state!!,dta.templateBtn?.fail?.get(0)?.icon!!,dta.templateBtn?.fail?.get(0)?.textKey!!,
+            dta.templateBtn?.fail?.get(1)?.state!!,dta.templateBtn?.fail?.get(1)?.icon!!,dta.templateBtn?.fail?.get(1)?.textKey!!)
+        else checkEnableOneButton(viewGroup,
+            dta.templateBtn?.fail?.get(0)?.state!!,dta.templateBtn?.fail?.get(0)?.icon!!,dta.templateBtn?.fail?.get(0)?.textKey!!)
+
+
+        if(dta.templateBtn?.cancel?.size!!>1)
+            checkEnableTwoButton(viewGroup,
+            dta.templateBtn?.cancel?.get(0)?.state!!,dta.templateBtn?.cancel?.get(0)?.icon!!,dta.templateBtn?.cancel?.get(0)?.textKey!!,
+            dta.templateBtn?.fail?.get(1)?.state!!,dta.templateBtn?.cancel?.get(1)?.icon!!,dta.templateBtn?.cancel?.get(1)?.textKey!!)
+        else checkEnableOneButton(viewGroup,
+            dta.templateBtn?.cancel?.get(0)?.state!!,dta.templateBtn?.cancel?.get(0)?.icon!!,dta.templateBtn?.cancel?.get(0)?.textKey!!)
     }
 
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-    override fun checkEnableTwoButton(viewGroup: ViewGroup, state1:String ,state2:String ) {
-      if (state1.equals("enable")==true && state2.equals("enable")==true) setTwoButton(viewGroup)
+    override fun checkEnableTwoButton(viewGroup: ViewGroup, state1: String, icon1: String, textKey1: String, state2: String, icon2: String, textKey2: String) {
+      if (state1.equals("enable")==true && state2.equals("enable")==true && flag ==0){
+          flag =1
+          setTwoButton(viewGroup,icon1,textKey1,icon2,textKey2)
+      }
     }
 
-    override fun checkEnableOneButton(viewGroup: ViewGroup, state: String) {
-
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    override fun checkEnableOneButton(viewGroup: ViewGroup, state: String, icon: String, textKey: String) {
+        if (state.equals("enable")==true&& flag ==0){
+            flag =1
+            setOneButton(viewGroup,icon,textKey)
+        }
     }
-    override fun setOneButton(viewGroup: ViewGroup) {
-        val linearLayoutParent :LinearLayout = LinearLayout(context)
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    override fun setOneButton(viewGroup: ViewGroup, icon: String, textKey: String) {
+        val linearLayout :LinearLayout = LinearLayout(context)
         val paramParent = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT,1f)
-        linearLayoutParent.layoutParams = paramParent
-        linearLayoutParent.orientation = LinearLayout.HORIZONTAL
+        paramParent.setMargins(30,0,30,20)
+        linearLayout.layoutParams = paramParent
+        linearLayout.gravity = Gravity.CENTER_HORIZONTAL
+        linearLayout.orientation = LinearLayout.HORIZONTAL
 
-        val linearLayoutParent1 : LinearLayout = LinearLayout(context)
-        linearLayoutParent1.layoutParams = paramParent
-        linearLayoutParent1.setBackgroundColor(Color.parseColor("#000000"))
+        val parambt = LinearLayout.LayoutParams(150,150)
+        val paramtext = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT)
+        paramtext.setMargins(0,30,0,0)
+        val requestOptions = RequestOptions()
 
-        val linearLayoutParent2 : LinearLayout = LinearLayout(context)
-        linearLayoutParent1.layoutParams = paramParent
-        linearLayoutParent1.setBackgroundColor(Color.parseColor("#ffffff"))
+        val button : ImageView = ImageView(context)
+        button.layoutParams = parambt
+        button.scaleType = ImageView.ScaleType.FIT_CENTER
+        requestOptions.placeholder(R.mipmap.ic_launcher)
+        Glide.with(context).setDefaultRequestOptions(requestOptions).load(icon).into(button);
+        button.setOnClickListener(View.OnClickListener {
+            Toast.makeText(context,"action",Toast.LENGTH_SHORT).show()
+        })
 
-        val linearLayoutParent3 : LinearLayout = LinearLayout(context)
-        linearLayoutParent1.layoutParams = paramParent
-        linearLayoutParent1.setBackgroundColor(Color.parseColor("#000000"))
+        val textView : TextView = TextView(context)
+        textView.layoutParams = paramtext
+        textView.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+        textView.setText(textKey)
+        textView.setOnClickListener(View.OnClickListener {
+            Toast.makeText(context,"action",Toast.LENGTH_SHORT).show()
+        })
 
-        linearLayoutParent.addView(linearLayoutParent1)
-        linearLayoutParent.addView(linearLayoutParent2)
-        linearLayoutParent.addView(linearLayoutParent3)
+        linearLayout.addView(button)
+        linearLayout.addView(textView)
+
+        viewGroup.addView(linearLayout)
     }
 
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-    override fun setTwoButton(viewGroup: ViewGroup) {
+    override fun setTwoButton(viewGroup: ViewGroup, icon1: String, textKey1: String, icon2: String, textKey2: String) {
         val linearLayoutParent :LinearLayout = LinearLayout(context)
         val paramParent = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT,1.5f)
         paramParent.setMargins(30,0,30,20)
@@ -79,23 +136,13 @@ class JsonPresenterTemplateBtnImpl (var context: Context) : JsonInterfaceTemplat
         val linearLayoutParent1 : LinearLayout = LinearLayout(context)
         linearLayoutParent1.layoutParams = paramParent
         linearLayoutParent1.orientation = LinearLayout.HORIZONTAL
-        linearLayoutParent1.setOnClickListener(View.OnClickListener {
-            Log.e("1","1")
-        })
-
 
         val linearLayoutParent2 : LinearLayout = LinearLayout(context)
         linearLayoutParent2.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT,2f)
-        linearLayoutParent2.setOnClickListener(View.OnClickListener {
-            Log.e("2","2")
-        })
 
         val linearLayoutParent3 : LinearLayout = LinearLayout(context)
         linearLayoutParent3.layoutParams = paramParent
         linearLayoutParent3.orientation = LinearLayout.HORIZONTAL
-        linearLayoutParent3.setOnClickListener(View.OnClickListener {
-            Log.e("3","3")
-        })
 
         val parambt = LinearLayout.LayoutParams(150,150)
         val paramtext = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT)
@@ -104,10 +151,9 @@ class JsonPresenterTemplateBtnImpl (var context: Context) : JsonInterfaceTemplat
 
         val button1 : ImageView = ImageView(context)
         button1.layoutParams = parambt
-        val url1 : String?  = dta.templateBtn?.new?.get(0)?.icon
         button1.scaleType = ImageView.ScaleType.FIT_CENTER
         requestOptions.placeholder(R.drawable.btn_cancel)
-        Glide.with(context).setDefaultRequestOptions(requestOptions).load(url1).into(button1);
+        Glide.with(context).setDefaultRequestOptions(requestOptions).load(icon1).into(button1);
         button1.setOnClickListener(View.OnClickListener {
             Toast.makeText(context,"cancel",Toast.LENGTH_SHORT).show()
         })
@@ -115,7 +161,7 @@ class JsonPresenterTemplateBtnImpl (var context: Context) : JsonInterfaceTemplat
         val textView1 : TextView = TextView(context)
         textView1.layoutParams = paramtext
         textView1.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
-        textView1.setText(dta.templateBtn?.new?.get(0)?.textKey)
+        textView1.setText(textKey1)
         textView1.setOnClickListener(View.OnClickListener {
             Toast.makeText(context,"cancel",Toast.LENGTH_SHORT).show()
         })
@@ -123,17 +169,16 @@ class JsonPresenterTemplateBtnImpl (var context: Context) : JsonInterfaceTemplat
         val textView2 : TextView = TextView(context)
         textView2.layoutParams = paramtext
         textView2.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
-        textView2.setText(dta.templateBtn?.new?.get(1)?.textKey)
+        textView2.setText(textKey2)
         textView2.setOnClickListener(View.OnClickListener {
             Toast.makeText(context,"comfig",Toast.LENGTH_SHORT).show()
         })
 
         val button2 : ImageView = ImageView(context)
         button2.layoutParams = parambt
-        val url2 : String?  = dta.templateBtn?.new?.get(1)?.icon
         button2.scaleType = ImageView.ScaleType.FIT_CENTER
         requestOptions.placeholder(R.drawable.btn_confirm)
-        Glide.with(context).setDefaultRequestOptions(requestOptions).load(url2).into(button2);
+        Glide.with(context).setDefaultRequestOptions(requestOptions).load(icon2).into(button2);
         button2.setOnClickListener(View.OnClickListener {
             Toast.makeText(context,"comfig",Toast.LENGTH_SHORT).show()
         })
--- app/src/main/java/com/training/tinhla/training/nkhoi_srcollview/presenterInterface/JsonInterfaceTemplateButton.kt
+++ app/src/main/java/com/training/tinhla/training/nkhoi_srcollview/presenterInterface/JsonInterfaceTemplateButton.kt
@@ -5,9 +5,9 @@ import android.view.ViewGroup
 open class JsonInterfaceTemplateButton {
     interface presenterTemplateBnt{
         fun getListButton(viewGroup: ViewGroup)
-        fun setOneButton(viewGroup: ViewGroup)
-        fun setTwoButton(viewGroup: ViewGroup)
-        fun checkEnableOneButton(viewGroup: ViewGroup,state :String)
-        fun checkEnableTwoButton(viewGroup: ViewGroup,state1 :String,state2:String)
+        fun setOneButton(viewGroup: ViewGroup , icon : String , textKey :String)
+        fun setTwoButton(viewGroup: ViewGroup, icon1 : String , textKey1 :String, icon2 : String , textKey2 :String)
+        fun checkEnableOneButton(viewGroup: ViewGroup,state :String , icon : String , textKey :String)
+        fun checkEnableTwoButton(viewGroup: ViewGroup,state1 :String,icon1 : String , textKey1 :String,state2:String, icon2 : String , textKey2 :String)
     }
 }
